b73f5ecf7cbd78ff4906f6d4dab7e0a5bc8c585d
mac grewal <7713555+macgrewal@users.noreply.github.com>
2020-03-12 13:06:07 +0000

    Added Scratch Process service

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,19 +4,19 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-
-    "uk.gov.hmrc"             %% "govuk-template"           % "5.45.0-play-26",
-    "uk.gov.hmrc"             %% "play-ui"                  % "8.4.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0"
+    "uk.gov.hmrc" %% "govuk-template" % "5.45.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.4.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0" % Test classifier "tests",
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0" % Test classifier "tests",
+    "org.scalamock" %% "scalamock" % "4.4.0" % "test",
+    "org.scalatest" %% "scalatest" % "3.0.8" % "test",
+    "org.jsoup" % "jsoup" % "1.10.2" % "test",
+    "com.typesafe.play" %% "play-test" % current % "test",
+    "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
     "com.github.tomakehurst" % "wiremock" % "2.23.2" % "test, it"
   )
 
