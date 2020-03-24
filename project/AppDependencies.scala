a51f805d8f655b18c59413ba9afd43696aa0a7c7
mac grewal <7713555+macgrewal@users.noreply.github.com>
2020-03-24 15:34:28 +0000

    Uplifted dependencies and removed unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,17 +5,14 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "govuk-template" % "5.45.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "8.4.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.23.0-play-26"
+    "uk.gov.hmrc" %% "play-ui" % "8.8.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.5.0",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.26.0-play-26"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0" % Test classifier "tests",
-    "uk.gov.hmrc" %% "reactivemongo-test" % "4.16.0-play-26" % "test, it",
     "org.scalamock" %% "scalamock" % "4.4.0" % "test",
     "org.scalatest" %% "scalatest" % "3.0.8" % "test",
-    "org.jsoup" % "jsoup" % "1.10.2" % "test",
     "com.typesafe.play" %% "play-test" % current % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
