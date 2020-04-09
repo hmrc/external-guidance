4268bcefc72a163715f6caab2bdd6fb5cae7ea13
leswilson <les.wilson@capgemini.com>
2020-04-09 10:38:51 +0100

    Initial commit for SubmittedProcess processing - contains Model, Formatter (and test) and Repository classes.

---
+++ app/repositories/SubmittedRepository.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import javax.inject.{Inject, Singleton}
+import models.errors.{DatabaseError, Errors, NotFoundError}
+import models.{RequestOutcome, SubmittedProcess}
+import play.api.libs.json.{Format, JsObject}
+import play.modules.reactivemongo.ReactiveMongoComponent
+import repositories.formatters.SubmittedProcessFormatter
+import uk.gov.hmrc.mongo.ReactiveRepository
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+//$COVERAGE-OFF$
+
+trait SubmittedRepository {
+  def save(id: String, process: JsObject): Future[RequestOutcome[String]]
+  def getById(id: String): Future[RequestOutcome[JsObject]]
+}
+
+
+@Singleton
+class SubmittedRepositoryImpl @Inject() (mongoComponent: ReactiveMongoComponent)
+  extends ReactiveRepository[SubmittedProcess, String](
+    collectionName = "submittedProcesses",
+    mongo = mongoComponent.mongoConnector.db,
+    domainFormat = SubmittedProcessFormatter.mongoFormat,
+    idFormat = implicitly[Format[String]]
+  ) with SubmittedRepository {
+
+  def save(id: String, process: JsObject): Future[RequestOutcome[String]] = {
+
+    logger.info(s"Saving process $id to collection submitted")
+
+    val document: SubmittedProcess = SubmittedProcess(id, process)
+
+    insert(document)
+      .map { _ =>
+        Right(document.id)
+      }
+      .recover {
+        case error =>
+          logger.error(s"Attempt to persist process $id to collection submitted failed with error : ${error.getMessage}")
+          Left(Errors(DatabaseError))
+      }
+  }
+
+  def getById(id: String): Future[RequestOutcome[JsObject]] = {
+
+    findById(id)
+      .map {
+        case Some(submittedProcess) => Right(submittedProcess.process)
+        case None => Left(Errors(NotFoundError))
+      }
+      .recover {
+        case error =>
+          logger.error(s"Attempt to retrieve process $id from collection submitted failed with error : ${error.getMessage}")
+          Left(Errors(DatabaseError))
+      }
+  }
+
+}
+
+//$COVERAGE-ON$
---
+++ test/repositories/formatters/SubmittedProcessFormatterSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories.formatters
+
+import base.UnitSpec
+import models.SubmittedProcess
+import play.api.libs.json.{JsError, JsObject, JsSuccess, Json}
+
+class SubmittedProcessFormatterSpec extends UnitSpec {
+
+  private val process: JsObject = Json.obj()
+  private val id: String = "ext90002"
+
+  private val submittedProcess: SubmittedProcess = SubmittedProcess(id, process)
+
+  private val json = Json.parse(
+    s"""
+       |{
+       | "_id": "$id",
+       | "process": {}
+       |}
+       |""".stripMargin
+  )
+
+  private val invalidJson = Json.parse("{}")
+
+  "Deserializing a JSON payload into an instance of SubmittedProcess" should {
+
+    "Result in a successful conversion for valid JSON" in {
+
+      json.validate[SubmittedProcess](SubmittedProcessFormatter.mongoFormat) match {
+        case JsSuccess(result, _) if result == submittedProcess => succeed
+        case JsSuccess(_, _) => fail("Deserializing valid JSON did not create correct process")
+        case _ => fail("Unable to parse valid Json")
+      }
+    }
+
+    "Result in a failure when for invalid JSON" in {
+
+      invalidJson.validate[SubmittedProcess](SubmittedProcessFormatter.mongoFormat) match {
+        case e: JsError => succeed
+        case _ => fail("Invalid JSON payload should not have been successfully deserialized")
+      }
+    }
+
+  }
+
+  "Serializing a submitted process into JSON" should {
+
+    "Generate the expected JSON" in {
+      val result = Json.toJson(submittedProcess)(SubmittedProcessFormatter.mongoFormat)
+      result shouldBe json
+    }
+  }
+
+}
