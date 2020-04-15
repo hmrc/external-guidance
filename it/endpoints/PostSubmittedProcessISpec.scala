756dac48bfefe8cd9eca7b277a9de311d8edf333
LesWilson <1684505+LesWilson@users.noreply.github.com>
2020-04-15 09:03:15 +0100

    Integration tests for submit process

---
+++ it/endpoints/PostSubmittedProcessISpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package endpoints
+
+import java.util.UUID
+
+import data.ExamplePayloads
+import play.api.http.{ContentTypes, Status}
+import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.ws.WSResponse
+import stubs.AuditStub
+import support.IntegrationSpec
+
+class PostSubmittedProcessISpec extends IntegrationSpec {
+
+  "Calling the submitted POST endpoint with a valid payload" should {
+
+    val randomId = UUID.randomUUID().toString
+    lazy val request = buildRequest("/external-guidance/submit")
+    lazy val response: WSResponse = {
+      AuditStub.audit()
+      val processToSave: JsObject = Json.obj("meta" -> Json.obj("id"-> randomId))
+      await(request.post(processToSave))
+    }
+
+    "return a CREATED status code" in {
+      response.status shouldBe Status.CREATED
+    }
+
+    "return content as JSON" in {
+      response.contentType shouldBe ContentTypes.JSON
+    }
+
+    "return an id property in the response" in {
+      val json = response.body[JsValue].as[JsObject]
+      json.keys should contain("id")
+    }
+
+    "return an id value based on the content of the JSON payload" in {
+      val json = response.body[JsValue].as[JsObject]
+      val id = (json \ "id").as[String]
+      id shouldBe randomId
+    }
+  }
+}
