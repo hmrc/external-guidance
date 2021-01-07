d7e29e7d236152b31a1e33e5e7a3d2f3977aa379
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-01-07 11:27:40 +0000

    EG-1179, new tests

--- test/models/ocelot/LabelSpec.scala
+++ test/models/ocelot/LabelSpec.scala
@@ -18,9 +18,12 @@ package models.ocelot
 
 import base.BaseSpec
 import play.api.libs.json._
+import play.api.i18n.Lang
 
 class LabelSpec extends BaseSpec with ProcessJson {
 
+  val englishLang: Lang = Lang("en")
+  val welshLang: Lang = Lang("cy")
   val label = """{"name":"BLAH"}"""
   val labelWithSingleValue = """{"name":"BLAH","english":"39.99"}"""
   val labelWithValues = """{"name":"BLAH","english":"Hello","welsh":"Welsh, Hello"}"""
@@ -68,9 +71,42 @@ class LabelSpec extends BaseSpec with ProcessJson {
       val labelsMap = Map("X"->Label("X", Some("33.5")), "Y"->Label("Y", Some("4")), "Name" -> Label("Name", Some("Coltrane")))
       val labels = LabelCache(labelsMap)
       labels.value("X") shouldBe Some("33.5")
-
       labels.value("Name") shouldBe Some("Coltrane")
+    }
 
+    "Return a language specifc value of a multi-lingual label" in {
+      val labelsMap = Map("Door"-> Label("Door", Some("Open"), Some("Drws")), "Name" -> Label("Name", Some("Coltrane")))
+      val labels = LabelCache(labelsMap)
+      labels.displayValue("Door")(englishLang) shouldBe Some("Open")
+      labels.displayValue("Door")(welshLang) shouldBe Some("Drws")
+      labels.displayValue("Name")(englishLang) shouldBe Some("Coltrane")
+      labels.displayValue("Name")(welshLang) shouldBe Some("Coltrane")
+    }
+
+    "Allow access to the main label map" in {
+      val labelsMap = Map("Door"-> Label("Door", Some("Open"), Some("Drws")), "Name" -> Label("Name", Some("Coltrane")))
+      val labels = LabelCache(labelsMap)
+
+      labels.labelMap shouldBe labelsMap
+    }
+
+    "Allow addition/update of labels with an english and welsh translation" in {
+      val labels = LabelCache(Map("Door"-> Label("Door", Some("Open"), Some("Drws")), "Name" -> Label("Name", Some("Coltrane"))))
+
+      val updatedLabels0 = labels.update("Door", "Ajar", "Dysgu")
+      updatedLabels0.displayValue("Door")(englishLang) shouldBe Some("Ajar")
+      updatedLabels0.displayValue("Door")(welshLang) shouldBe Some("Dysgu")
+
+      val updatedLabels1 = updatedLabels0.update("Door", "Open", "Drws")
+      updatedLabels1.displayValue("Door")(englishLang) shouldBe Some("Open")
+      updatedLabels1.displayValue("Door")(welshLang) shouldBe Some("Drws")
+    }
+
+    "Construct a LabelCache from a label map and a cache of updated labels" in {
+      val labelsMap = Map("X"->Label("X", Some("33.5")), "Y"->Label("Y"), "Name" -> Label("Name", Some("Coltrane")))
+      val cacheMap = Map("X"->Label("X", Some("46.5")))
+      val labels = LabelCache(labelsMap, cacheMap)
+      labels.value("X") shouldBe Some("46.5")
     }
 
     "Return an empty string if label has no assigned value" in {
@@ -91,9 +127,7 @@ class LabelSpec extends BaseSpec with ProcessJson {
       labels.value("X") shouldBe Some("33.5")
 
       val updatedLabels = labels.update("Name", "Miles")
-
       updatedLabels.value("Name") shouldBe Some("Miles")
-
     }
 
     "Allow a new label to be added to the cache" in {
@@ -102,21 +136,17 @@ class LabelSpec extends BaseSpec with ProcessJson {
       labels.value("X") shouldBe Some("33.5")
 
       val updatedLabels = labels.update("Location", "Here")
-
       updatedLabels.value("Location") shouldBe Some("Here")
-
     }
 
     "Return a map of new and updated labels on request" in {
       val labelsMap = Map("X"->Label("X", Some("33.5")), "Y"->Label("Y", Some("4")), "Name" ->Label("Name", Some("Coltrane")))
       val labels = LabelCache(labelsMap)
       labels.value("X") shouldBe Some("33.5")
+
       val labels1 = labels.update("X", "49.5")
-
       val labels2 = labels1.update("Location", "Here")
-
       labels2.updatedLabels shouldBe Map("X" ->Label("X",Some("49.5")), "Location" ->Label("Location",Some("Here")))
-
     }
 
     "Flush updated labels to main store" in {
@@ -125,18 +155,12 @@ class LabelSpec extends BaseSpec with ProcessJson {
 
       val labels1 = labels.update("X", "49.5")
       val labels2 = labels1.update("Location", "Here")
-
       labels2.updatedLabels shouldBe Map("X" ->Label("X",Some("49.5")), "Location" ->Label("Location",Some("Here")))
 
       val labels3 = labels2.flush
-
       labels3.updatedLabels shouldBe Map()
-
       labels3.value("X") shouldBe Some("49.5")
       labels3.value("Location") shouldBe Some("Here")
-
     }
-
   }
-
 }
