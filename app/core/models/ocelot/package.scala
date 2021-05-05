e466baf6d086d1abd7b1023363c0c6496afdb056
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-05-05 10:25:29 +0100

    EG-1386, fix all link regex

--- app/core/models/ocelot/package.scala
+++ app/core/models/ocelot/package.scala
@@ -26,7 +26,7 @@ package object ocelot {
   val boldPattern: String = s"\\[bold:($labelPattern|[^\\]]+)\\]"
   val linkToPageOnlyPattern: String = s"\\[link:(.+?):(\\d+|${Process.StartStanzaId})\\]"
   val pageLinkPattern: String = s"\\[(button|link)(-same|-tab)?:(.+?):(\\d+|${Process.StartStanzaId})\\]"
-  val buttonLinkPattern: String = s"\\[(button)(-same|-tab)?:([^\\]]+?):(\\d+|${Process.StartStanzaId})\\]"
+  val buttonLinkPattern: String = s"\\[(button)(-same|-tab)?:(.+?):(\\d+|${Process.StartStanzaId})\\]"
   val linkPattern: String = s"\\[(button|link)(-same|-tab)?:(.+?):(\\d+|${Process.StartStanzaId}|https?:[a-zA-Z0-9\\/\\.\\-\\?_\\.=&#]+)\\]"
   val commaSeparatedIntsPattern: String = "^\\d+\\s*(?:,\\s*\\d+\\s*)*$"
   val listPattern: String = "\\[list:([A-Za-z0-9\\s\\-_]+):length\\]"
