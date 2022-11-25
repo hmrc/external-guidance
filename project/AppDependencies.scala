622bf0d444efe6ffe81394e1ee7ee3ccd82f8435
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-11-25 14:41:50 +0000

    DL-8973, dependency udpates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.25.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.71.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.74.0",
     "uk.gov.hmrc"       %% "auth-client"               % "5.14.0-play-28"
   )
 
@@ -13,7 +13,7 @@ object AppDependencies {
     "org.scalamock"                %% "scalamock"               % "5.2.0"  % "test",
     "org.scalatest"                %% "scalatest"               % "3.1.4"  % "test",
     "com.typesafe.play"            %% "play-test"               % current  % "test",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28" % "0.71.0" % "test",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28" % "0.74.0" % "test",
     "org.pegdown"                  %  "pegdown"                 % "1.6.0"  % "test, it",
     "org.scalatestplus.play"       %% "scalatestplus-play"      % "5.1.0"  % "test, it",
     "com.github.tomakehurst"       %  "wiremock-jre8"           % "2.33.2" % "test, it",
