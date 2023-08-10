c237ee50e04638da673a3d156a697e64bad87b83
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-08-10 16:41:01 +0100

    DL-10770, dependency updates

--- app/core/models/MongoDateTimeFormats.scala
+++ app/core/models/MongoDateTimeFormats.scala
@@ -48,9 +48,23 @@ trait MongoDateTimeFormats {
   final val tolerantLocalDateFormat: Format[LocalDate] =
     Format(tolerantLocalDateReads, MongoJavatimeFormats.localDateWrites)
 
+  // LocalDateTime
+
+  final val localDateTimeReads: Reads[LocalDateTime] =
+    Reads.at[String](__ \ "$date" \ "$numberLong")
+      .map(dateTime => Instant.ofEpochMilli(dateTime.toLong).atZone(ZoneOffset.UTC).toLocalDateTime)
+
+  final val localDateTimeWrites: Writes[LocalDateTime] =
+    Writes.at[String](__ \ "$date" \ "$numberLong")
+      .contramap(_.toInstant(ZoneOffset.UTC).toEpochMilli.toString)
+
+  final val localDateTimeFormat: Format[LocalDateTime] =
+    Format(localDateTimeReads, localDateTimeWrites)
+
+
   trait Implicits {
     implicit val mdInstantFormat: Format[Instant] = MongoJavatimeFormats.instantFormat
-    implicit val mdLocalDateTimeFormat: Format[LocalDateTime] = MongoJavatimeFormats.localDateTimeFormat
+    implicit val mdLocalDateTimeFormat: Format[LocalDateTime] = localDateTimeFormat
     implicit val mdLocalDateFormat: Format[LocalDate] = tolerantLocalDateFormat
     implicit val mdZonedDateTimeFormat: Format[ZonedDateTime] = zonedDateTimeFormat
   }
