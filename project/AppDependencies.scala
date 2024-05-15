882f6af2152315ecd730f2ac7c41b4b4b7e4d4a7
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2024-05-15 14:04:54 +0100

    DL-13758, dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,9 @@ import sbt.*
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "8.5.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "8.6.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "1.9.0",
-    "uk.gov.hmrc"       %% "auth-client"               % "7.1.0",
+    "uk.gov.hmrc"       %% "auth-client"               % "8.0.0",
     "org.typelevel"     %% "cats-core"                 % "2.10.0"
   )
 
@@ -13,6 +13,6 @@ object AppDependencies {
     "org.scalamock"                %% "scalamock"               % "5.2.0"  % "test",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30" % "1.9.0" % "test",
     "org.scalatestplus.play"       %% "scalatestplus-play"      % "5.1.0"  % "test",
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"  % "8.5.0" % "test"
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"  % "8.6.0" % "test"
   )
 }
