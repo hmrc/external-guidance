f34ec60ce1823ae7f6ab06c155c284e6eca12b67
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-07-31 17:29:06 +0100

    DL-14198 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,20 @@ import sbt.*
 
 object AppDependencies {
 
+  val bootstrapVersion = "9.1.0"
+  val mongoPlayVersion = "2.2.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "9.0.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.0.0",
-    "uk.gov.hmrc"       %% "auth-client"               % "8.0.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % mongoPlayVersion,
+    "uk.gov.hmrc"       %% "auth-client"               % "8.2.0",
     "org.typelevel"     %% "cats-core"                 % "2.12.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalamock"                %% "scalamock"               % "5.2.0"  % Test,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30" % "2.0.0" % Test,
-    "org.scalatestplus.play"       %% "scalatestplus-play"      % "7.0.1"  % Test,
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"  % "9.0.0" % Test
-  )
+    "org.scalamock"          %% "scalamock"               % "5.2.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlayVersion,
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion
+  ).map(_ % Test)
 }
