df11eb8f1d4b38d4f7766534be020144acfe20fc
preetampotdar <10691333+preetampotdar@users.noreply.github.com>
2025-02-19 13:39:16 +0000

    chore(DL-15502): Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,13 +18,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.8.0"
+  val bootstrapVersion = "9.9.0"
   val mongoPlayVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % mongoPlayVersion,
-    "uk.gov.hmrc"       %% "auth-client"               % "8.4.0",
+    "uk.gov.hmrc"       %% "auth-client"               % "8.5.0",
     "org.typelevel"     %% "cats-core"                 % "2.13.0"
   )
 
