7d7b062ac96c52fef1832271635c7e0c401ecf03
Byasar3 <byasar3@users.noreply.github.com>
2025-04-10 10:48:58 +0100

    DL-15793: dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,17 +19,17 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion = "9.11.0"
-  val mongoPlayVersion = "2.5.0"
+  val mongoPlayVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % mongoPlayVersion,
-    "uk.gov.hmrc"       %% "auth-client"               % "8.5.0",
+    "uk.gov.hmrc"       %% "auth-client"               % "8.6.0",
     "org.typelevel"     %% "cats-core"                 % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalamock"          %% "scalamock"               % "6.2.0",
+    "org.scalamock"          %% "scalamock"               % "7.3.0",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlayVersion,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion
